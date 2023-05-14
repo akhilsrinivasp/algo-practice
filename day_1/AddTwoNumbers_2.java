297991f94e58b0a85c7752328dce49ff0825b864
Akhil Srinivas P <akhilsrinivasp@gmail.com>
2023-05-14 13:32:53 +0530

    LongestSubstringWithoutRepeatingCharacters done

--- day_1/AddTwoNumbers_2.java
+++ day_1/AddTwoNumbers_2.java
@@ -25,3 +25,10 @@ class Solution {
         return d.next;
     }
 }
+
+/*
+ * Time complexity : O(max(m,n)). Assume that m and n represents the length of
+ * l1 and l2 respectively, the algorithm above iterates at most max(m,n) times.
+ * Space complexity : O(max(m,n)). The length of the new list is at most
+ * max(m,n)+1.
+ */
---
+++ day_1/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -0,0 +1,54 @@
+package day_1;
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters_3 {
+    
+}
+
+// Time Optimized:
+class Solution_0 {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==0) return 0;
+        int i =0; 
+        int j =0;
+        int m =1;
+        HashMap<Character, Integer> mp = new HashMap<>();
+        while(j<s.length()) {
+            if(mp.containsKey(s.charAt(j))) i = Math.max(mp.get(s.charAt(j)),i);
+            m = Math.max(m, j-i+1);
+            mp.put(s.charAt(j), j+1);
+            ++j;
+        }
+        return m;
+    }
+}
+
+// Memory Optimized:
+class Solution_1 {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==0) return 0;
+        int i =0; 
+        int j =1;
+        int m =1;
+        while(i<s.length() && j<s.length()) {
+            for(int k = i; k<j; k++) {
+                if(s.charAt(j)==s.charAt(k)) 
+                    i = k+1;
+            }
+            m = Math.max(m, j-i+1);
+            ++j;
+        }
+        return m;
+    }
+}
+
+/*
+ * Solution_0:
+ *  Time complexity : O(n). Index j will iterate n times.
+ *  Space complexity (HashMap) : O(min(m,n)). Same as the previous approach.
+ *  Space complexity (Table): O(m). m is the size of the charset.
+ * 
+ * Solution_1:
+ *  Time complexity : O(n^2). Index j will iterate n times.
+ *  Space complexity (Table): O(m). m is the size of the charset.
+ */
