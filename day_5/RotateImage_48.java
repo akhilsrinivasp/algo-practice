1971357042e7c47798388a4ff521a4ecf87e3740
Akhil Srinivas P <akhilsrinivasp@gmail.com>
2023-05-19 23:18:08 +0530

    day 5 mod

--- day_5/RotateImage_48.java
+++ day_5/RotateImage_48.java
@@ -23,13 +23,13 @@ class Solution {
             }
         }
     }
-    private void printMatrix(int[][] m) {
-        for(int i=0; i<m.length; i++) {
-            for(int j=0; j<m[i].length; j++) 
-                System.out.print(m[i][j] + " ");
-            System.out.println();
-        }
-    }
+    // private void printMatrix(int[][] m) {
+    //     for(int i=0; i<m.length; i++) {
+    //         for(int j=0; j<m[i].length; j++) 
+    //             System.out.print(m[i][j] + " ");
+    //         System.out.println();
+    //     }
+    // }
 }
 
 class Solution_0 {
